fca95719ade5a94bbbd96a2095dc82a112f5b36c
Huyen <huyen5c13>
2015-04-14 12:36:48 +0700

    Updated java

--- huyen/JavaApplication1.java
+++ huyen/JavaApplication1.java
@@ -1,23 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaapplication1;
-
-/**
- *
- * @author Thanh
- */
 public class JavaApplication1 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("hello world ");
-        
-// TODO code application logic here
+        System.out.println("something");
     }
     
 }
