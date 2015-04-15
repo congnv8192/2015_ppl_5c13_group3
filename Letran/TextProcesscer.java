525efb8737bc37f9c843ef5975b3a4e8b6b9ee6d
LuffyTanmeo <funnycat131195@gmail.com>
2015-04-15 23:45:14 +0700

    Trần Thị Mỹ Lệ

---
+++ Letran/TextProcesscer.java
@@ -0,0 +1,25 @@
+/**
+     * read file, remove all the redundant spaces, uppercase first letter of each word and lowercase other.
+     * @require filepath!=null
+     * @modifies
+     *  remove redundant spaces in content 
+     *  foreach word in content
+     *      capitalize first letter
+     *      lowercase remaining letters
+     *  returns content
+     */
+    public static String task2(String filepath){
+        
+}
+
+   /**
+    * read file, count lines and words, write update content into "data2.txt"
+    * @require filepath!=null
+    * @modifie
+    * @effect
+    * read file content into String content
+    * count lines and words
+    * write update content into "data2.txt"
+    */
+ public static String task3(String filepath){
+    }
